f916866e43f6238dfcc4d260e5165256bbb6f3f9
akdesignstudio <akalvania@gmail.com>
2025-10-28 17:53:27 +0530

    Replace 'Testimonials' with 'Case Study' in nav items

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
     { name: 'Work', href: '#portfolio' },
     { name: 'About', href: '#about' },
     { name: 'Process', href: '#process' },
-    { name: 'Testimonials', href: '#testimonials' },
+    { name: 'Case Study', href: '#casestudy' },
     { name: 'Contact', href: '#contact' },
   ];
 
