11ae4861e02451c87802ae9af4fea5b636a036d7
akdesignstudio <akalvania@gmail.com>
2025-10-29 17:02:53 +0530

    Update image URLs in testimonials data

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -22,7 +22,7 @@ export const testimonials: Testimonial[] = [
     role: "Product Designer",
     company: "CIN7 India Pvt Ltd",
     quote: "CIN7 Core is a cloud-based ERP platform designed for businesses that need centralized control over orders, inventory, purchasing, warehouses, and fulfillment. My involvement focused on automation workflows, improving the orders, inventory modules filters on the listing, and layout improvements.",
-    imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pencilpay.com%2Fus%2Fcin7-core-us%2F&psig=AOvVaw32ELmlg4NMbdWNxxOv5-hA&ust=1761823196756000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOCutJ-lyZADFQAAAAAdAAAAABAK"
+    imageUrl: "https://www.pencilpay.com/wp-content/uploads/2024/06/Cin7Core-1-1.png"
   },
   {
     id: 3,
@@ -30,6 +30,6 @@ export const testimonials: Testimonial[] = [
     role: "Senior Product Designer",
     company: "OpenXcell Pvt. Ltd.",
     quote: "Orderhive is a centralized order, inventory, warehouse, and shipping management system designed to automate workflows across eCommerce, wholesale, and retail channels. My involvement focused on designing optimized workflows, improving the order processing experience, and enabling fast dispatch operations through UI setup, automation, and layout improvements.",
-    imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Foman.whizzcart.com%2Fproduct%2F25584001%2Forderhive-inventory-management-software-free-trial-available%2F&psig=AOvVaw1BpujnLoKuIMMlNXLVxi_s&ust=1761823313429000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKDRir6lyZADFQAAAAAdAAAAABA9"
+    imageUrl: "https://images-na.ssl-images-amazon.com/images/I/41xOUk0NIEL._UL1200_.jpg"
   }
 ];
